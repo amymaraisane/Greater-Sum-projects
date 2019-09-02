85cc7970e470a4a104d03796718cdd2947b21d81
amymaraisane <amy.maraisane@gmail.com>
2019-09-02 18:12:17 -0400

    phone number validator project first commit

---
+++ TDD-Phone Number Validator/spec/phoneNumSpec.js
@@ -0,0 +1,22 @@
+describe('Phone Number Validator', () => {
+    it('checks for 10 values present', ()=>{
+        expect(phoneNumChecker('5555555555')).toBe(true)
+        expect(phoneNumChecker('555555555')).toBe(false)
+    });
+    it('checks that each value is a number', ()=>{
+        expect(phoneNumChecker('555555555a')).toBe(false)
+    })
+    // it('can include a space after 3rd number and 6th number', ()=>{
+    //     expect(phoneNumChecker('222 222 2222')).toBe(true)
+    //     expect(phoneNumChecker('222-222-2222')).toBe(true)
+    //     expect(phoneNumChecker('2 22222 2222')).toBe(false)
+    // })
+    // it('can have parens around area code then no space', ()=>{
+    //     expect(phoneNumChecker('(222)222-2222')).toBe(true)
+    //     expect(phoneNumChecker('(222) 222 2222')).toBe(true)
+    //     expect(phoneNumChecker('(222)222 2222')).toBe(false)
+    //     expect(phoneNumChecker('(222)2222222')).toBe(false)
+    // })
+});
+
+
---
+++ TDD-Phone Number Validator/src/phoneNum.js
@@ -0,0 +1,30 @@
+const phoneNumChecker = (number)=>{
+    const numArr = number.split("")
+    if (number.length < 10){
+        return false; 
+    }
+    let nonNumeric = numArr.filter(i=>{
+        return (/\D/.test(i))
+    })
+    if (nonNumeric.length > 0){
+        return false;
+    }
+    // if(number.length === 12){
+    //     if (numArr[3] !== " " || numArr[7] !== " "){
+    //         debugger;
+    //         return false
+    //     }
+    // }
+    //     //write the long if statement, then refactor
+    //     checkSpot(numArr[3]) || checkSpot(numArr[7]) === false ? false : true  
+    return true;
+}
+
+//write the long if statement, then refactor
+
+// const checkSpot = value =>{
+//     if (value !== " " || value !== "-"){
+//         return false
+//     }
+//     return true
+// }
