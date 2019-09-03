851f9472bcb7d9138e7ad4e77da1364660779743
amymaraisane <amy.maraisane@gmail.com>
2019-09-02 21:57:31 -0400

    start again using regex

--- TDD-Phone Number Validator/src/phoneNum.js
+++ TDD-Phone Number Validator/src/phoneNum.js
@@ -1,31 +1,6 @@
 const phoneNumChecker = (number)=>{
     const numArr = number.split("")
-    if (number.length < 10){
-        return false;  
-    }
-    if(number.length >=10){
-        let noSpaces = number.replace(/\s+/g, "")
-        noSpaces = noSpaces.split("").filter(i=> /\D/.test(i))
-        if (noSpaces.length > 0){
-            return false;
-        }
-    }
-    if(number.length === 12){
-        if ((numArr[3] !== " ") || (numArr[7] !== " ")){
-            debugger;
-            return false
-        }
-    }
-    //     //write the long if statement, then refactor
-    //     checkSpot(numArr[3]) || checkSpot(numArr[7]) === false ? false : true  
-    return true;
+    const numberDigits = parseInt(number, 10)
+    result = /[0-9]/.test(numberDigits)
+    return result
 }
-
-//write the long if statement, then refactor
-
-// const checkSpot = value =>{
-//     if (value !== " " || value !== "-"){
-//         return false
-//     }
-//     return true
-// }
