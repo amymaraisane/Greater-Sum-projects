fe7c820c2e6d3e950d790545d17d00277e9e85ef
amymaraisane <amy.maraisane@gmail.com>
2019-09-14 12:17:32 -0400

    change names for describe block and first few tests, start from blank source

--- TDD-Phone Number Validator/src/phoneNum.js
+++ TDD-Phone Number Validator/src/phoneNum.js
@@ -1,12 +1,2 @@
-const phoneNumChecker = (number)=>{
-    if (number.length === 10){
-        let result = /[0-9]{10}/.test(number)
-        return result
-    }
-    if (number.length >=12){
-        let result = /\(?[\d]{3}\)?[\s-]?[\d]{3}[\s-]?[\d]{4}/.test(number)
-        return result
-    }
-    return false
-}
+
 
